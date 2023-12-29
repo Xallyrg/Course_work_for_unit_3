9618a4cd926c03f71e44d743e0226b9d3378ef3b
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2023-12-29 03:08:32 +0300

    Fix comments to functions

--- course-work-for-unit-3/func.py
+++ course-work-for-unit-3/func.py
@@ -5,7 +5,7 @@ import operator
 
 def get_operations_from_json(filename):
     """
-    По json файлу делает список операций
+    По json файлу делает список словарей (операций)
     :param filename: название файла
     :return: список операций
     """
@@ -17,12 +17,11 @@ def get_operations_from_json(filename):
         return list_of_operations
 
 
-
 def get_executed_operations(operations):
     """
     Функция оставляет только совершенные операции, из списка всех операций
-    :param operations:
-    :return:
+    :param operations: список операций
+    :return: список свершенных операций
     """
     operation_list = []
     for operation in operations:
@@ -30,36 +29,30 @@ def get_executed_operations(operations):
             operation_list.append(operation)
     return operation_list
 
-# print(get_executed_operations(get_operations_from_json('operations.json')))
-
-
 
 def sort_operations_by_date(operations):
     """
     Функция сортирует по дате полученный массив операций
-    :param operations:
-    :return:
+    :param operations: список операций
+    :return: список операций отсортированный по дате
     """
     sorted_operations = sorted(operations, key=lambda x: x['date'], reverse=True)
 
     return sorted_operations
+
 
 def get_new_operations(filename, quantity):
     """
     Берет список операций из файла, удаляет не исполненные операции, сортирует оставшиеся операции по дате и выводит quantity последних операций
     :param filename: название файла откуда берем данные
     :param quantity: количество операций, которые надо вывести
-    :return:
+    :return: Возвращает quantity последних операций, отсортированных по дате
     """
     list_of_operations = get_operations_from_json(filename)
     executed_operations = get_executed_operations(list_of_operations)
     s_sorted = sort_operations_by_date(executed_operations)
 
     return s_sorted[0:quantity]
-
-
-# print(get_new_operations('operations.json', 2))
-
 
 
 def get_date_of_operation(operation):
@@ -77,14 +70,11 @@ def get_date_of_operation(operation):
         return 'Дата не указана'
 
 
-
-
-
 def get_encrypted_from_number(operation):
     """
-    Делает по номеру карты из операции шифрованный номер для вывода
-    :param operation:
-    :return:
+    Делает по номеру from из операции шифрованный номер для вывода
+    :param operation: операция
+    :return: номер карты/счера откуда перевод в нужном формате
     """
     from_info = operation.get('from')
 
@@ -105,9 +95,9 @@ def get_encrypted_from_number(operation):
 
 def get_encrypted_to_number(operation):
     """
-    Делает по номеру счета из операции шифрованный номер для вывода
-    :param operation:
-    :return:
+    Делает по номеру to из операции шифрованный номер для вывода
+    :param operation: операция
+    :return: номер карты/счера куда перевод в нужном формате
     """
     to_info = operation.get('to')
 
@@ -129,18 +119,20 @@ def get_encrypted_to_number(operation):
 def info_about_operation(operation):
     """
     Функция которая по операции готовит вывод информации
-    :param operation: словарь с данными по операции
-    :return: строка в читаемом виде
+    :param operation: операция
+    :return: строка в нужном виде
     """
     operation_decription = operation.get('description') if operation.get('description') else "Нет описания перевода"
 
     operation_operationAmount = operation.get('operationAmount')
     if operation_operationAmount:
-        operation_operationAmount_amount = operation_operationAmount.get('amount') if operation_operationAmount.get('amount') else "Сумма не указана"
+        operation_operationAmount_amount = operation_operationAmount.get('amount') if operation_operationAmount.get(
+            'amount') else "Сумма не указана"
 
         operation_operationAmount_currency = operation_operationAmount.get("currency")
         if operation_operationAmount_currency:
-            operation_operationAmount_currency_name = operation_operationAmount_currency.get("name") if operation_operationAmount_currency.get("name") else "Валюта не указана"
+            operation_operationAmount_currency_name = operation_operationAmount_currency.get(
+                "name") if operation_operationAmount_currency.get("name") else "Валюта не указана"
         else:
             operation_operationAmount_currency_name = "Валюта не указана"
     else:
@@ -152,4 +144,3 @@ def info_about_operation(operation):
                 operation_operationAmount_amount + ' ' + operation_operationAmount_currency_name + '\n')
 
     return info_str
-
